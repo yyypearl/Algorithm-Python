8be7223a4fe6991fae3577e7df79dff3e0b1f569
JIN JU YOO <102593738+yyypearl@users.noreply.github.com>
2024-11-19 01:50:36 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def fibo(n):
+    if (n==0):
+        return 0
+    elif (n==1):
+        return 1
+    else:
+        return fibo(n-2) + fibo(n-1)
+    
+print(fibo(n))
