11a6e957feed510ae2e9827d96a4d64af5fbafea
JIN JU YOO <102593738+yyypearl@users.noreply.github.com>
2024-12-17 02:31:32 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -6,23 +6,21 @@ n, total = map(int, input().split())
 str = list(input().split())
 str.sort()
 
-path = [0] * n       # 뽑은 알파벳
-used = [0] * len(str) # 알파벳 사용 유무
+combi = [0] * n       # 뽑은 알파벳
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 def dfs(level, start):
     if level == n:
         v_count = 0
         for v in vowels:
-            if v in path:
+            if v in combi:
                 v_count += 1
         if(v_count >= 1 and n - v_count>=2):
-            print(*path, sep='')
+            print(*combi, sep='')
         return
     for i in range(start, len(str)):
-        if used[i] == 0:
-            path[level] = str[i]
-            dfs(level+1, i+1)
-            path[level] = 0
+        combi[level] = str[i]
+        dfs(level+1, i+1)
+        combi[level] = 0
 
 dfs(0, 0)
